39cea07fc6aaedd41e448a393d9c2959934acb51
Asra Faheem <asrafaheem124@gmail.com>
2025-06-19 21:40:28 +0500

    updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// appstyle class
 class AppStyles {
   static TextStyle heading = TextStyle(
     color: Color.fromARGB(0, 255, 255, 255),
@@ -9,8 +10,19 @@ class AppStyles {
   );
 
   static Color themeColor = Color(0xff2D201C);
+
+  static TextStyle featureHead = TextStyle(
+    fontSize: 14,
+    fontFamily: 'ProductSans',
+  );
+
+  static TextStyle featureText = TextStyle(
+    fontSize: 14,
+    fontFamily: 'ProductSans-b',
+  );
 }
 
+// button
 class MyButton extends StatelessWidget {
   final title;
   final route;
@@ -19,31 +31,40 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 200,
-        height: 60,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
-        border: Border.all(color: Colors.white, width: 2)
+      width: 200,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        border: Border.all(color: Colors.white, width: 2),
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.grey.withOpacity(0.9),
         ),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.grey.withOpacity(0.9)),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return route;
-            }));
-          },
-          child: Text(
-            title,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'ProductSans',
-              fontSize: 18,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return route;
+              },
             ),
+          );
+        },
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'ProductSans',
+            fontSize: 18,
           ),
         ),
+      ),
     );
   }
 }
 
+//button
 class ButtonForm extends StatelessWidget {
   final title;
   const ButtonForm({super.key, required this.title});
@@ -51,30 +72,30 @@ class ButtonForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 130,
-        height: 50,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
-        ),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Color(0xff2D201C)),
-          onPressed: () {
-            // Navigator.push(context, MaterialPageRoute(builder: (context){
-            //   return route;
-            // }));
-          },
-          child: Text(
-            title,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'ProductSans-b',
-              fontSize: 16,
-            ),
+      width: 130,
+      height: 50,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Color(0xff2D201C)),
+        onPressed: () {
+          // Navigator.push(context, MaterialPageRoute(builder: (context){
+          //   return route;
+          // }));
+        },
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'ProductSans-b',
+            fontSize: 16,
           ),
         ),
+      ),
     );
   }
 }
 
+//button
 class ConfirmButton extends StatelessWidget {
   const ConfirmButton({super.key});
 
@@ -85,11 +106,88 @@ class ConfirmButton extends StatelessWidget {
       height: 50,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-        onPressed: (){}, child: Text('Confirm',  style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'ProductSans-b',
-              fontSize: 18,
-            ),))
+        onPressed: () {},
+        child: Text(
+          'Confirm',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'ProductSans-b',
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// card
+class MyCard extends StatelessWidget {
+  final image;
+  final title;
+  final price;
+  const MyCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 172,
+          width: 120,
+          decoration: BoxDecoration(
+            color: Color(0xffE4E1E8),
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(title, style: AppStyles.featureHead),
+        SizedBox(height: 5),
+        Text(price, style: AppStyles.featureText),
+      ],
+    );
+  }
+}
+
+//heading section
+class MySection extends StatelessWidget {
+  final heading;
+  final button;
+  const MySection({super.key, required this.heading, required this.button});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              heading,
+              style: TextStyle(fontSize: 20, fontFamily: 'ProductSans-b'),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              button,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'ProductSans',
+                fontSize: 13,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -24,7 +24,7 @@ class HomeScreen extends StatelessWidget {
         drawer: Drawer(),
         body: Column(
           children: [
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             TabBar(
               labelColor: AppStyles.themeColor,
               unselectedLabelColor: Colors.grey,
@@ -37,36 +37,28 @@ class HomeScreen extends StatelessWidget {
                     padding: const EdgeInsets.all(4.0),
                     child: CircleAvatar(
                       backgroundColor: AppStyles.themeColor,
-                      child: Icon(
-                        Icons.woman,
-                        size: 30,
-                        color: Colors.white,
-                      ),
+                      child: Icon(Icons.woman, size: 30, color: Colors.white),
                     ),
                   ),
                   text: 'Women',
                 ),
-                 Tab(
+                Tab(
                   height: 70,
                   icon: Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: CircleAvatar(
-                      backgroundColor:AppStyles.themeColor,
-                      child: Icon(
-                        Icons.man,
-                        size: 30,
-                        color: Colors.white,
-                      ),
+                      backgroundColor: AppStyles.themeColor,
+                      child: Icon(Icons.man, size: 30, color: Colors.white),
                     ),
                   ),
                   text: 'Men',
                 ),
-                 Tab(
+                Tab(
                   height: 70,
                   icon: Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: CircleAvatar(
-                      backgroundColor:AppStyles.themeColor,
+                      backgroundColor: AppStyles.themeColor,
                       child: Icon(
                         Icons.design_services,
                         size: 30,
@@ -74,10 +66,10 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  text: 'Design'
+                  text: 'Design',
                 ),
-                 Tab(
-                 height: 70,
+                Tab(
+                  height: 70,
                   icon: Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: CircleAvatar(
@@ -89,27 +81,226 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  text: 'Beauty'
+                  text: 'Beauty',
                 ),
               ],
             ),
-            Expanded(child: TabBarView(children: [
-              Center(
-                  child: Column(
-                    children: [
-                      SizedBox(height: 30,),
-                     Container(
-                      height: 200,
-                      width: 380,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(image: AssetImage('assets/images/home_banner.png'),fit: BoxFit.cover,),
-                        borderRadius: BorderRadius.circular(20)  
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(height: 30),
+                          Container(
+                            height: 200,
+                            width: 340,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(
+                                  'assets/images/home_banner.png',
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 20,
+                                top: 10,
+                              ),
+                              child: Text(
+                                'Autumn \nCollection \n2025',
+                                textAlign: TextAlign.end,
+                                style: TextStyle(
+                                  fontSize: 26,
+                                  fontFamily: 'ProductSans-b',
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          MySection(
+                            heading: 'Featured Products',
+                            button: 'Show all',
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  MyCard(
+                                    image: 'assets/images/feature-1.png',
+                                    title: 'Turtleneck Sweater',
+                                    price: '\$39.99',
+                                  ),
+                                  SizedBox(width: 20,),
+                                  MyCard(
+                                    image: 'assets/images/feature-2.png',
+                                    title: 'Long Sleeve Dress',
+                                    price: '\$40.00',
+                                  ),
+                                  SizedBox(width: 20,),
+                                  MyCard(
+                                    image: 'assets/images/feature-3.png',
+                                    title: 'Sportswear Set',
+                                    price: '\$55.00',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Container(
+                            width: double.infinity,
+                            height: 171,
+                            color: Color(0xffF8F8FA),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '| New Collection',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontFamily: 'ProductSans-l',
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'HANG OUT \n& PARTY',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: 'ProductSans-l',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Image.asset('assets/images/home_banner2.png'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          MySection(heading: 'Recommended', button: 'Show all'),
+                          SizedBox(height: 10),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 213,
+                                    height: 66,
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffF9F9F9),
+                                      borderRadius: BorderRadius.circular(10)
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Column(
+                                          children: [
+                                           ClipRRect(
+                                            borderRadius: BorderRadiusGeometry.circular(10),
+                                            child:  Image.asset(
+                                              'assets/images/recom-1.png',
+                                            ),
+                                           )
+                                          ],
+                                        ),
+                                        SizedBox(width: 10),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              'White Fashion Hoodie',
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                fontFamily: 'ProductSans',
+                                              ),
+                                            ),
+                                            Text(
+                                              '\$29.00',
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontFamily: 'ProductSans-b',
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(width: 20,),
+                                  Container(
+                                    width: 213,
+                                    height: 66,
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffF9F9F9),
+                                      borderRadius: BorderRadius.circular(20)
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Column(
+                                          children: [
+                                            ClipRRect(
+                                            borderRadius: BorderRadiusGeometry.circular(10),
+                                            child:  Image.asset(
+                                              'assets/images/recom-2.png',
+                                            ),
+                                           )
+                                          ],
+                                        ),
+                                        SizedBox(width: 10),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              'Cotton T-shirt',
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                fontFamily: 'ProductSans',
+                                              ),
+                                            ),
+                                            Text(
+                                              '\$29.00',
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontFamily: 'ProductSans-b',
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                     )
-                    ],
+                    ),
                   ),
-                ),
-            ]))
+                  Expanded(child: Text('second')),
+                  Expanded(child: Text('third')),
+                  Expanded(child: Text('fourth')),
+                ],
+              ),
+            ),
           ],
         ),
       ),
